0c0ddc4e34ae3a11aba5a9c66af7f107c15531b8
Ghabry <gabriel+github@mastergk.de>
2021-07-21 16:03:01 +0200

    Simplify convert script, CMake and Gradle makes this much easier

--- convert_script.sh
+++ convert_script.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 set -e
 
 # Standalone game variables
@@ -11,14 +13,10 @@ GAME_VERSION_NAME=1.0.0
 GAME_METADATA=$(pwd)/metadata
 GAME_ICON_PATH=$GAME_METADATA/en-US/images/icon.png
 GAME_ICON_PROMOGRAPHIC=$GAME_METADATA/en-US/images/promoGraphic.png
+EASYRPG_PLAYER_FOLDER=$(pwd)/buildscripts/android/Player
 ##############################
 
-EASYRPG_BUILD_ANDROID=$(pwd)/buildscripts/android
-EASYRPG_PLAYER_FOLDER=$EASYRPG_BUILD_ANDROID/Player
 ANDROID_FOLDER=$EASYRPG_PLAYER_FOLDER/builds/android
-#GAME_APK_FOLDER_NAME=$(echo "$GAME_APK_NAME" | sed 's/\./\//g')
-GAME_APK_FOLDER_NAME="com/your/game"
-GAME_APK_NATIVE=$(echo "$GAME_APK_NAME" | sed 's/\./_/g')
 
 cd $EASYRPG_PLAYER_FOLDER
 
@@ -29,63 +27,20 @@ echo "" >> .gitignore
 echo "# Game folder" >> .gitignore
 echo "/builds/android/app/src/main/assets/" >> .gitignore
 
-cd src/platform/android
-
-if [ ! -f $GAME_APK_NATIVE'_player_EasyRpgPlayerActivity.cpp' ]; then
-  git mv 'org_easyrpg_player_player_EasyRpgPlayerActivity.cpp' $GAME_APK_NATIVE'_player_EasyRpgPlayerActivity.cpp'
-  git mv 'org_easyrpg_player_player_EasyRpgPlayerActivity.h' $GAME_APK_NATIVE'_player_EasyRpgPlayerActivity.h'
-  find . -type f -name "*.cpp" -exec sed -i "s|org_easyrpg_player|$GAME_APK_NATIVE|g" {} \;
-  find . -type f -name "*.h" -exec sed -i "s|org_easyrpg_player|$GAME_APK_NATIVE|g" {} \;
-fi
-
-cd ../../../
-
-find . -type f -name "CMakeLists.txt" -exec sed -i "s|org_easyrpg_player|$GAME_APK_NATIVE|g" {} \;
-find . -type f -name "CMakeLists.txt" -exec sed -i "s|org.easyrpg.player|$GAME_APK_NAME|g" {} \;
-
 cd builds/android/app/src/main
 
 # Copy game
 rm -fr assets/game
 cp -r $GAME_FOLDER assets/game
 
-# Change java packages
-if [ ! -d java/$GAME_APK_FOLDER_NAME ]; then
-  echo "hohoh"
-  mkdir -p java/$GAME_APK_FOLDER_NAME
-  git mv java/org/easyrpg/player/* java/$GAME_APK_FOLDER_NAME
-fi
-
-cd ..
-
-# Change jni filenames
-if [ ! -f 'gamebrowser/'$GAME_APK_NATIVE'_game_browser_GameScanner.cpp' ]; then
-  git mv 'gamebrowser/org_easyrpg_player_game_browser_GameScanner.cpp' 'gamebrowser/'$GAME_APK_NATIVE'_game_browser_GameScanner.cpp'
-  git mv 'gamebrowser/org_easyrpg_player_game_browser_GameScanner.h' 'gamebrowser/'$GAME_APK_NATIVE'_game_browser_GameScanner.h'
-  git mv 'jni/src/org_easyrpg_player_player_EasyRpgPlayerActivity.cpp' 'jni/src/'$GAME_APK_NATIVE'_player_EasyRpgPlayerActivity.cpp'
-  git mv 'jni/src/org_easyrpg_player_player_EasyRpgPlayerActivity.h' 'jni/src/'$GAME_APK_NATIVE'_player_EasyRpgPlayerActivity.h'
-fi
-
-# Change jni references
-find . -type f -name "*.cpp" -exec sed -i "s|org_easyrpg_player|$GAME_APK_NATIVE|g" {} \;
-find . -type f -name "*.h" -exec sed -i "s|org_easyrpg_player|$GAME_APK_NATIVE|g" {} \;
-find . -type f -name "*.mk" -exec sed -i "s|org_easyrpg_player|$GAME_APK_NATIVE|g" {} \;
-find . -type f -name "CMakeLists.txt" -exec sed -i "s|org_easyrpg_player|$GAME_APK_NATIVE|g" {} \;
-
-cd main
-
 # Change APK name
-find . -type f -name "*.java" -exec sed -i "s|org\.easyrpg\.player|$GAME_APK_NAME|g" {} \;
-find . -type f -name "*.xml" -exec sed -i "s|org\.easyrpg\.player|$GAME_APK_NAME|g" {} \;
 sed -i "s|org\.easyrpg\.player|$GAME_APK_NAME|g" $ANDROID_FOLDER/app/build.gradle
-sed -i "s|org\.easyrpg\.player|$GAME_APK_NAME|g" $ANDROID_FOLDER/fastlane/Appfile
-sed -i "s|org\.easyrpg\.player|$GAME_APK_NAME|g" AndroidManifest.xml
 
 # Change game name
 sed -i "s|EasyRPG Player|$GAME_NAME|g" res/values/strings.xml
 
 # Change game email
-sed -i "s|easyrpg@easyrpg\.org|$GAME_BUG_REPORT_EMAIL|g" java/$GAME_APK_FOLDER_NAME/player/EasyRpgPlayerActivity.java
+sed -i "s|easyrpg@easyrpg\.org|$GAME_BUG_REPORT_EMAIL|g" java/org/easyrpg/player/player/EasyRpgPlayerActivity.java
 
 # Change game site
 sed -i "s|https://easyrpg\.org/|$GAME_SITE|g" res/layout/browser_nav_header.xml
